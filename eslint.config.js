d7f8e0bd29f24fdbd616c4ce37b54c0c4c3250f8
Luis Zampieron <luisfernandozampieron@gmail.com>
2026-04-09 21:37:57 -0300

    chore: configurar eslint, prettier, husky e lint-staged

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  {
+    ignores: ["node_modules/**"],
+  },
+  {
+    files: ["eslint.config.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        require: "readonly",
+        module: "readonly",
+      },
+    },
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        localStorage: "readonly",
+        fetch: "readonly",
+        setTimeout: "readonly",
+        confirm: "readonly",
+        bootstrap: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
